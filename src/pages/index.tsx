f4ccbf8ba915a279adc83c6b3698b135d9bce3de
Saleel <saleel@saleel.xyz>
2023-03-29 23:45:01 +0530

    chore: add eslint, commitlint and prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -12,5 +12,5 @@ export default function Home() {
         <div>Zuzalu Feedback</div>
       </main>
     </>
-  )
+  );
 }
