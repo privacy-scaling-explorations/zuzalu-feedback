20d72e3358c6bfcd80536c896445efd5002d9525
Saleel <saleel@saleel.xyz>
2023-03-29 23:23:21 +0530

    chore: add base styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
+        <div>Zuzalu Feedback</div>
       </main>
     </>
   )
