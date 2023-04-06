afffbb17035655a544edcc8799dd86d4e681ec47
cedoor <me@cedoor.dev>
2023-04-06 18:07:43 +0100

    chore: add supabase url as an env variable

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://bbqpmdghoyettdhwrgss.supabase.co";
+const supabaseUrl = process.env.SUPABASE_URL as string;
 const supabaseKey = process.env.SUPABASE_KEY as string;
 
 export default createClient(supabaseUrl, supabaseKey);
