ce26c9012c6e5d378e6352175daef6e578b8fdcf
cedoor <me@cedoor.dev>
2023-04-19 16:14:54 +0100

    feat: integrate pwa conf and app icons

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const withPWA = require("next-pwa");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development"
+})({
   reactStrictMode: true
-};
+});
 
 module.exports = nextConfig;
