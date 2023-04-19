1a7b88a8caffda1fe4b214b27abd98ada41afb6d
cedoor <me@cedoor.dev>
2023-04-19 16:23:43 +0100

    chore: add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,8 +10,29 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Zuzalu Feedback</title>
-        <meta name="description" content="Anonymous feedback app for Zuzalu.city" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta name="application-name" content="Zuzalu Feedback" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Zuzalu Feedback" />
+        <meta name="description" content="An app to allow Zuzalu attendees to send anonymous feedback." />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#1c2928" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Zuzalu Feedback" />
+        <meta property="og:description" content="An app to allow Zuzalu attendees to send anonymous feedback." />
+        <meta property="og:site_name" content="Zuzalu Feedback" />
+        <meta property="og:url" content="https://zuzalu-feedback.appliedzkp.org" />
+
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
       </Head>
       <main className={font.className}>
         <Component {...pageProps} />
