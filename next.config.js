84af1eeeaa971d4a4044f97798084c9011283adf
cedoor <me@cedoor.dev>
2023-05-02 13:12:28 +0100

    chore: set explicit env variables

--- next.config.js
+++ next.config.js
@@ -5,7 +5,18 @@ const nextConfig = withPWA({
   dest: "public",
   disable: process.env.NODE_ENV === "development"
 })({
-  reactStrictMode: true
+  reactStrictMode: true,
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY
+  },
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_PASSPORT_URL: process.env.NEXT_PUBLIC_PASSPORT_URL,
+    NEXT_PUBLIC_ZUZALU_SEMAPHORE_GROUP_URL: process.env.NEXT_PUBLIC_ZUZALU_SEMAPHORE_GROUP_URL,
+    NEXT_PUBLIC_ZUZALU_SEMAPHORE_GROUP_ROOT_URL: process.env.NEXT_PUBLIC_ZUZALU_SEMAPHORE_GROUP_ROOT_URL,
+    NEXT_PUBLIC_ZUZALU_FEEDBACK_APP_ORIGIN: process.env.NEXT_PUBLIC_ZUZALU_FEEDBACK_APP_ORIGIN,
+    NEXT_PUBLIC_ZUZALU_API: process.env.NEXT_PUBLIC_ZUZALU_API
+  }
 });
 
 module.exports = nextConfig;
